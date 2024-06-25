e382d3da2c52c0f3982e2f0ce4b0942852eb366a
Арсений <s3v3nny@ya.ru>
2024-06-25 11:22:05 +0400

    irz practice

--- endpoint.py
+++ endpoint.py
@@ -1,20 +1,26 @@
 import os
 import json
+import socket
 
-json_file = open("config.json")
-server_data = json.load(json_file)
+server = socket.socket()
+hostname = socket.gethostname()
+port = 9090
+server.bind((hostname, port))
 
-service_list = []
+while True:
+    server.accept()
+    json_file = open("config.json")
+    json_data = json.load(json_file)
 
-for s in server_data['servers']:
-    command = 'systemctl is-active ' + s['service_name']
-    service_status = os.system(command)
+    status_list = []
+    for s in json_data['servers']:
+        command = 'systemctl is-active ' + s['service_name']
+        service_status = os.system(command)
 
-    if service_status == "active":
-        service_list.append({"service": s['name'], "status": "active"})
+        if service_status == "active":
+            status_list.append({"service": s['name'], "status": "active"})
 
-    if service_status == "inactive":
-        service_list.append({"service": s['name'], "status": "inactive"})
+        if service_status == "inactive":
+            status_list.append({"service": s['name'], "status": "inactive"})
 
-
-print(service_list)
+    print(json.dump(status_list))
