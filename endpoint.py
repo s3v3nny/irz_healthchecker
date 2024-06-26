33c9a5a5f79de46af10c5d26deedd94d3e92d009
Арсений <s3v3nny@ya.ru>
2024-06-26 09:48:21 +0400

    irz practice

--- endpoint.py
+++ endpoint.py
@@ -1,10 +1,13 @@
-import os
+import http.server
 import json
+import os
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+class RequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
         json_file = open("config.json")
         json_data = json.load(json_file)
         status_list = []
@@ -13,15 +16,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             service_status = os.system(command)
 
             if service_status == "active":
-                status_list.append({'service_name': s['name'], 'status': "active"})
+                status_list.append({'service_name': s['name'],
+                                    'status': "active"})
 
             if service_status == "inactive":
-                status_list.append({'service_name': s['name'], 'status': "inactive"})
+                status_list.append({'service_name': s['name'],
+                                    'status': "inactive"})
         json_file.close()
         self.wfile.write(json.dumps(status_list).encode())
-        self.send_response(200)
 
 
-
-httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
+httpd = http.server.HTTPServer(('', 8080), RequestHandler)
 httpd.serve_forever()
