85fbf2b53c2cd7d2e90e439b08f17b396d19adf4
Арсений <s3v3nny@ya.ru>
2024-06-26 10:21:05 +0400

    irz practice

--- endpoint.py
+++ endpoint.py
@@ -10,21 +10,19 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         json_file = open("config.json")
         json_data = json.load(json_file)
-        output_data = []
         services = {'services': []}
-        output_data.append(services)
         for s in json_data['servers']:
             command = 'systemctl is-active ' + s['service_name']
             status = subprocess.getoutput(command)
 
             if status == "active":
                 print("active here")
-                services.append({'service_name': s['name'],
+                services['services'].append({'service_name': s['name'],
                                     'status': "active"})
 
             if status == "inactive":
                 print("inactive here")
-                services.append({'service_name': s['name'],
+                services['services'].append({'service_name': s['name'],
                                     'status': "inactive"})
         json_file.close()
         print(json.dumps(services))
